4ce9a5ca43def0bbef65fc5100059bf321890f60
Michael Nguyen <michaelnguyenm@gmail.com>
2017-07-02 02:05:36 -0500

    Made a button and form that work based on events

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -4,6 +4,10 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
+    // styles: [`
+    //     h1 {
+    //         color: dodgerblue
+    //     }`]
 })
 export class AppComponent {
     title = 'Seasonal Jpop';
