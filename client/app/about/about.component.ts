9f5b6359e077b2497a3b7302162d7e243405ac92
Michael Nguyen <michaelnguyenm@gmail.com>
2017-08-13 23:09:34 -0500

    Made about page

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  host: string = "moc.liamg";
+  user: string = "mneyugnleahcim";
+  at: string = "@";
+  mr: string = this.rev(this.host + this.at + this.user);
+
+  // .state('contact', {
+  //     url: '/contact',
+  //     templateUrl: 'contact.html',
+  //     controller: function($scope) {
+  //         // Obfuscation for fun
+  //         $scope.host = "moc.liamg";
+  //         $scope.user = "mneyugnleahcim";
+  //         $scope.at = "@";
+  //         // Based on http://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
+  //         function rev(x) { return x.split('').reverse().join(''); }
+  //         $scope.mr = rev($scope.host + $scope.at + $scope.user);
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  rev(toReverse: string): string {
+    return toReverse.split('').reverse().join('');
+  }
 }
